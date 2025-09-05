decfcd1e16d259c0cce921d9c4462d138edb4e56
agorapilled <johngardner13@outlook.com>
2025-09-05 19:46:56 -0400

    v0

--- src/app/components/AnimatedMarquee.js
+++ src/app/components/AnimatedMarquee.js
@@ -1,24 +1,24 @@
+import { memo } from 'react';
+import Marquee from "react-fast-marquee";
+
 const AnimatedMarquee = () => {
   return (
-    <div className="absolute top-0 index-0 w-screen text-sm py-[2px] text-white overflow-hidden">
-      <div className="flex w-max animate-marquee hover:pause">
-        <div className="flex-shrink-0 whitespace-nowrap pr-8">
-          Powerpump is a fully automated lottery protocol built on&nbsp;
-          <a className="text-blue-500 underline" href="https://pump.fun">pump.fun</a>. 
-          Users who hold the $POWER token are automatically eligible for the pump jackpot. 
-          Users have a weight assigned to them based on how much they hold relative to others. 
-          Fully transparent, equitable, and fair. Happy pumping!&nbsp;
-        </div>
-        <div className="flex-shrink-0 whitespace-nowrap pr-8" aria-hidden="true">
-          Powerpump is a fully automated lottery protocol built on&nbsp;
-          <a className="text-blue-500 underline" href="https://pump.fun">pump.fun</a>. 
-          Users who hold the $POWER token are automatically eligible for the pump jackpot. 
-          Users have a weight assigned to them based on how much they hold relative to others. 
-          Fully transparent, equitable, and fair. Happy pumping!&nbsp;
-        </div>
-      </div>
+    <div className="absolute top-0 index-0 w-screen text-sm py-[2px] text-white">
+      <Marquee speed={100}>
+        Powerpump is a fully automated lottery protocol built on&nbsp;
+        <a className="text-blue-500 underline" href="https://pump.fun">pump.fun</a>. 
+        Users who hold the $POWER token are automatically eligible for the pump jackpot. 
+        Users have a weight assigned to them based on how much they hold relative to others. 
+        Fully transparent, equitable, and fair. Happy pumping!&nbsp;
+        Powerpump is a fully automated lottery protocol built on&nbsp;
+        <a className="text-blue-500 underline" href="https://pump.fun">pump.fun</a>. 
+        Users who hold the $POWER token are automatically eligible for the pump jackpot. 
+        Users have a weight assigned to them based on how much they hold relative to others. 
+        Fully transparent, equitable, and fair. Happy pumping!&nbsp;
+      </Marquee>
     </div>
   );
 };
 
-export default AnimatedMarquee;
+// Wrap with React.memo to prevent re-renders
+export default memo(AnimatedMarquee);
