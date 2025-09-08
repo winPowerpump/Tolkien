0b0e44edc975ace3c44282855a8bd4c5a81440cb
agorapilled <johngardner13@outlook.com>
2025-09-08 04:12:04 -0400

    v0

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AnimatedMarquee from "./components/AnimatedMarquee";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Analytics />
       <AnimatedMarquee />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
